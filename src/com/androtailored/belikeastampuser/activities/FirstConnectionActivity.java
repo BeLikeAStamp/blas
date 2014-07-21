e326f4d57fe272a582e87e3013dd1b9094e3d955
andronekomimi <lemacon.audrey@gmail.com>
2014-07-21 23:20:25 +0200

    Replacement de l'appel a un thread secondaire par une asynctask chargée de l'enregistrement.

--- src/com/androtailored/belikeastampuser/activities/FirstConnectionActivity.java
+++ src/com/androtailored/belikeastampuser/activities/FirstConnectionActivity.java
@@ -1,7 +1,11 @@
 package com.androtailored.belikeastampuser.activities;
 
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources.NotFoundException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -18,7 +22,7 @@ public class FirstConnectionActivity extends Activity {
 	Button register;
 	EditText firstname, name, phone, email;
 	Long id = Long.valueOf(-1);
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -48,32 +52,44 @@ public class FirstConnectionActivity extends Activity {
 						&& phone.getText().toString().length() != 0
 						&& email.getText().toString().length() != 0
 						) {
-						
+
 					User user =  new User(firstname.getText().toString(), 
 							name.getText().toString(), phone.getText().toString(), 
 							email.getText().toString());
-					
-					if(registrationSuccessed(user))
-					{
-						Toast.makeText(getApplicationContext(), R.string.reg_succed, Toast.LENGTH_SHORT).show();
-						getSharedPreferences("REGISTERED USER", MODE_PRIVATE).edit()
-										.putBoolean("registered", true).commit();
-						Intent i = new Intent(FirstConnectionActivity.this, MainActivity.class);
-						i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-						startActivity(i);
+
+					try {
+						if(new AddUserTask().execute(user).get())
+						{
+							Toast.makeText(getApplicationContext(), R.string.reg_succed, Toast.LENGTH_SHORT).show();
+							getSharedPreferences("REGISTERED USER", MODE_PRIVATE).edit()
+							.putBoolean("registered", true).commit();
+							getSharedPreferences("USER ID", MODE_PRIVATE).edit().putLong("user_id", id).commit();
+							Intent i = new Intent(FirstConnectionActivity.this, MainActivity.class);
+							i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+							startActivity(i);
+						}
+						else
+						{
+							Toast.makeText(getApplicationContext(), R.string.reg_failed, Toast.LENGTH_SHORT).show();
+						}
+					} catch (NotFoundException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (ExecutionException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
 					}
-					else
-					{
-						Toast.makeText(getApplicationContext(), R.string.reg_failed, Toast.LENGTH_SHORT).show();
-					}
-						
+
 				}
 				else
 				{
 					Toast.makeText(getApplicationContext(), R.string.warning_reg, Toast.LENGTH_SHORT).show();
 				}
 
-				
+
 			}
 		});
 	}
@@ -93,45 +109,39 @@ public class FirstConnectionActivity extends Activity {
 
 		return isRegistered;
 	}
-	
-	private boolean registrationSuccessed(User user) {
-		addUser();
-		if(id.equals(Long.valueOf(-1))) return false;
-		
-		return true;
-	}
-	
-	final void addUser() {
-		final Thread checkUpdate = new Thread() {
-			public void run() {
 
-				User u = new User(firstname.getText().toString(),name.getText().toString(),
-						phone.getText().toString(), email.getText().toString());
-				u.setAddress("");
-				u.setIsHost(false);
-				u.setIsPartener(false);
-				
-				final UserController c = new UserController();
-				try {
-					// check user id d'abord...
+	private class AddUserTask extends AsyncTask<User, Void, Boolean> {
+
+		@Override
+		protected Boolean doInBackground(User... params) {
+			// TODO Auto-generated method stub
+			User u = params[0];
+			u.setAddress("");
+			u.setIsHost(false);
+			u.setIsPartener(false);
+
+			final UserController c = new UserController();
+			try {
+				// check user id d'abord...
+				id = c.getUserId(u.getEmail());
+				if(id.equals(Long.valueOf(-1))) {
+					Log.d("addUser", "unknown user");
+					c.create(u);
 					id = c.getUserId(u.getEmail());
-					if(id.equals(Long.valueOf(-1))) {
-						Log.d("addUser", "unknown user");
-						c.create(u);
-						id = c.getUserId(u.getEmail());
-					}
-					else
-						Log.d("addUser", "already known user");
-				} catch (Exception e) {
-					e.printStackTrace();
-					return;
+					Log.d("addUser", "user id = "+id);
 				}
-				/*final Intent intent = new Intent(AddUserActivity.this,
-						GetAllUserActivity.class);
-				startActivity(intent);*/
+				else
+					Log.d("addUser", "already known user");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		};
-		checkUpdate.start();
+
+			if(id.equals(Long.valueOf(-1)))
+				return false;
+			else
+				return true;
+		}
+
 	}
 
 }
